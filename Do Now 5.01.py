d6ec75bfe085a9620f986c6654878137f0125e39
peyton <peytonharris87@gmail.com>
2022-05-04 09:56:02 -0500

    added file

---
+++ Do Now 5.01.py
@@ -0,0 +1,42 @@
+'''
+############
+Do Now 5.01
+############
+
+In your Console
+---------------
+Type the following code:
+
+my_dictionary = {
+'cat': 'a domestic feline',
+'dog': 'a domestic canine',
+'chair': 'furniture piece for sitting',
+'car': 'automobile'
+}
+print(my_dictionary)
+print(my_dictionary['dog'])
+print(my_dictionary.get('dog'))
+print('cat' in my_dictionary)
+print('monkey' in my_dictionary)
+
+In your Notebook
+----------------
+Respond to the following:
+
+Write down what was printed out. What type is my_dictionary? it printed the dictionary, class type is dictionary type
+
+Add a line of code that will print the definition of 'chair', then run the code again. def of chair printed
+
+Write down what happens if you use my_dictionary['kittens']? What do you think that error means? the error means key kittens that kittens wasnt in the list
+'''
+my_dictionary = {
+'cat': 'a domestic feline',
+'dog': 'a domestic canine',
+'chair': 'furniture piece for sitting',
+'car': 'automobile'
+}
+print(my_dictionary)
+print(my_dictionary['dog'])
+print(my_dictionary.get('dog'))
+print('cat' in my_dictionary)
+print('monkey' in my_dictionary)
